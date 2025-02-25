ce75ce5c5a68c14fd3e3e1108b249431d1ba2a7b
Anusid W <anusid.w@ku.th>
2025-02-25 01:37:35 +0000

    bug fix

--- models.py
+++ models.py
@@ -8,7 +8,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.Text), nullable=False)
+    password = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now)
 
     def __repr__(self):
